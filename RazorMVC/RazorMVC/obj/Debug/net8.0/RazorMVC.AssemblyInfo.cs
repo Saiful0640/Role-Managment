679bf40fef8122eeaae853abfc9c33834916dc3c
Saiful0640 <saiful.amt.nift@gmail.com>
2024-09-18 17:23:14 +0600

    a

--- RazorMVC/RazorMVC/obj/Debug/net8.0/RazorMVC.AssemblyInfo.cs
+++ RazorMVC/RazorMVC/obj/Debug/net8.0/RazorMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16e4c7358246cbbece69d883a5b33a6a6ddee790")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bedbdb9e6307ea65ed2d7b19de54bfda3c304fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
